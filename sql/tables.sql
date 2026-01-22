1687672dcdf3fb8f4df308ecd6546fa119de2bc7
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2026-01-22 18:45:04 -0500

    Update tables.sql

--- sql/tables.sql
+++ sql/tables.sql
@@ -3,3 +3,24 @@ create or replace TABLE DEMO.DEMO.RAWSLACKIMAGES (
 	FILENAME VARCHAR(16777216),
 	JSON_DATA TEXT
 );
+
+
+create or replace ICEBERG TABLE DEMO.DEMO.SLACKIMAGES (
+	UUID VARCHAR,
+    messagerealname VARCHAR,
+	FILENAME VARCHAR,
+    messagechannel VARCHAR,
+    messageusertz VARCHAR,
+    messagefiletype VARCHAR,
+	IMAGE_TEXT TEXT,
+    `IMAGE` TEXT,
+    messageusername VARCHAR,
+    messagetimestamp VARCHAR,
+	ts VARCHAR
+)
+CATALOG = 'SNOWFLAKE';
+
+
+select * from DEMO.DEMO.SLACKIMAGES;
+
+select * from demo.demo.RAWSLACKIMAGES;
